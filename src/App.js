2b753892fa99be229e0f14e6af586347fee56ae1
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-03 11:21:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, useThree, extend } from "react-three-fiber";
+
+import "./App.css";
+
+extend({ OrbitControls });
+
+function Cube(props) {
+  const [isBig, setIsBig] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const size = isBig ? 2 : 1;
+  const color = isHovered ? "pink" : "salmon";
+
+  return (
+    <mesh
+      {...props}
+      onClick={() => setIsBig(!isBig)}
+      onPointerOver={() => setIsHovered(true)}
+      onPointerOut={() => setIsHovered(false)}
+    >
+      <boxBufferGeometry attach="geometry" args={[size, size, size]} />
+      <meshStandardMaterial attach="material" color={color} />
+    </mesh>
+  );
+}
+// args are [width, height , depth]
+
+function Scene() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  return (
+    <>
+      <ambientLight />
+      <pointLight intensity={0.3} position={[-1, 2, 4]} />
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
+      <orbitControls args={[camera, domElement]} />
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Scene />
+    </Canvas>
   );
 }
 
